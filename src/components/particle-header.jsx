2fd00a5ef87c929de8ac036dad13e137df1704d6
abimbola7 <abimbolababajide6@gmail.com>
2023-12-13 08:00:10 +0100

    fixes

--- src/components/particle-header.jsx
+++ src/components/particle-header.jsx
@@ -239,8 +239,8 @@ export default function ParticleHeader() {
     // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
     // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull (engine);
-    // await loadSlim(engine);
+    // await loadFull (engine);
+    await loadSlim(engine);
 }, []);
 
 const particlesLoaded = useCallback(async container => {
